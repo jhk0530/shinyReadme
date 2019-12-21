6a13ad75e4ea3a8734c1259b63492677496aaf07
jhk0530 <hwanistic@gmail.com>
2019-12-21 20:55:29 +0900

    Create functions.R

---
+++ inst/shinyReadme/functions.R
@@ -0,0 +1,31 @@
+library(shinyjs)
+
+addinTextArea <- function(txt) {
+  txt <- gsub("\n", "<br>", txt)
+  txt <- paste0("document.getElementById('text').value += '", txt, "'")
+  return(txt)
+}
+
+nextLine <- function() {
+  shinyjs::runjs("document.getElementById('text').value += '\\n'")
+}
+
+hide <- function(btn) {
+  shinyjs::runjs(paste0('$("#', btn, '").hide(500)'))
+}
+
+Add <- function(text) {
+  shinyjs::runjs(addinTextArea(text))
+}
+
+Focus <- function() {
+  shinyjs::runjs('$("#text").focus()')
+}
+
+CodeArea <- function(txt) {
+  Add("```r")
+  nextLine()
+  Add(txt)
+  nextLine()
+  Add("```")
+}
