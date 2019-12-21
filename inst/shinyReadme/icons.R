0031d8ca43d8d54c0378c607f48a8c0e1aa361c2
jhk0530 <hwanistic@gmail.com>
2019-12-21 20:55:23 +0900

    Create icons.R
    https://gist.github.com/rxaviers/7360908

---
+++ inst/shinyReadme/icons.R
@@ -0,0 +1,15 @@
+icons = c('blush', 'smiley', 'relaxed','grin','grinning','yum',
+          'yellow_heart','blue_heart','purple_heart','heart','green_heart',
+          'heartpulse','two_hearts','sparkling_heart','star','notes',
+          'musical_note','love_letter','snowflake','zap','ocean',
+          'cherry_blossom','tulip','paw_prints','bouquet','four_leaf_clover',
+          'rose','sunflower','hibiscus','palm_tree','evergreen_tree','deciduous_tree',
+          'octocat','gift','balloon','sound','mag','mag_right','bulb','email',
+          'wrench','clipboard','page_with_curl','page_facing_up','pushpin',
+          'paperclip','pencil2','green_book','blue_book','orange_book','closed_book',
+          'newspaper','gem','dart','memo','book','art','tophat','tea','cocktail',
+          'icecream','cherries','apple','lemon','melon','tomato',
+          'rainbow','ship','rocket','airplane','bike','tram','car',
+          'bus','truck','bullettrain_side','beginner','black_circle','white_circle','red_circle',
+          'cool','ballot_bot_with_check','white_check_mark'
+)
